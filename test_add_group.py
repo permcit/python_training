3061b5152bc600c565bf99424590f5301015a296
Roman <ste7ww@gmail.com>
2025-03-25 21:29:01 +0700

    Внес правки, тест запускается за 15 секунд

--- test_add_group.py
+++ test_add_group.py
@@ -11,30 +11,45 @@ class TestAddGroup(unittest.TestCase):
 
     def test_add_group(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        self.open_home_page(wd)
+        self.login(wd)
+        # password send
+        wd.find_element(By.NAME, "pass").clear()
+        wd.find_element(By.NAME, "pass").send_keys("secret")
+        # click login
+        wd.find_element(By.XPATH, "//input[@value='Login']").click()
+        self.open_groups_page(wd)
+        self.init_group_creation(wd)
+        # return to groups page
+        wd.find_element(By.LINK_TEXT, "groups").click()
+        # logout
+        wd.find_element(By.LINK_TEXT, "Logout").click()
 
+    def init_group_creation(self, wd):
+        # init group creation
+        wd.find_element(By.NAME, "new").click()
+        # fill group form
+        wd.find_element(By.NAME, "group_name").clear()
+        wd.find_element(By.NAME, "group_name").send_keys("dfidifk")
+        wd.find_element(By.NAME, "group_header").clear()
+        wd.find_element(By.NAME, "group_header").send_keys("fjjsdf")
+        wd.find_element(By.NAME, "group_footer").clear()
+        wd.find_element(By.NAME, "group_footer").send_keys("dsfjjjsdf")
+        # submit group creation
+        wd.find_element(By.NAME, "submit").click()
+
+    def open_groups_page(self, wd):
+        # open group page
+        wd.find_element(By.LINK_TEXT, "groups").click()
+
+    def login(self, wd):
+        # login
         wd.find_element(By.NAME, "user").clear()
         wd.find_element(By.NAME, "user").send_keys("admin")
 
-        wd.find_element(By.NAME, "pass").clear()
-        wd.find_element(By.NAME, "pass").send_keys("secret")
-
-        wd.find_element(By.XPATH, "//input[@value='Login']").click()
-        wd.find_element(By.LINK_TEXT, "groups").click()
-        wd.find_element(By.NAME, "new").click()
-
-        wd.find_element(By.NAME, "group_name").clear()
-        wd.find_element(By.NAME, "group_name").send_keys("dfidifk")
-
-        wd.find_element(By.NAME, "group_header").clear()
-        wd.find_element(By.NAME, "group_header").send_keys("fjjsdf")
-
-        wd.find_element(By.NAME, "group_footer").clear()
-        wd.find_element(By.NAME, "group_footer").send_keys("dsfjjjsdf")
-
-        wd.find_element(By.NAME, "submit").click()
-        wd.find_element(By.LINK_TEXT, "groups").click()
-        wd.find_element(By.LINK_TEXT, "Logout").click()
+    def open_home_page(self, wd):
+        # open home page
+        wd.get("http://localhost/addressbook/")
 
     def is_element_present(self, how, what):
         try:
